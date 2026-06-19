3ad606dbf73f3f2d27a5aa7a19fa5779e1b24c39
Keshav Pareek <keshavpareek@Keshavs-MacBook-Air.local>
2026-06-19 18:02:03 +0530

    utils updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import dotenv from "dotenv"
 import moongoose from "mongoose";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path : './.env'
 });
-connectDB();
+
+
+
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error !!!",error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000,() => {
+        console.log(`server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log(`failed to connect MongoDB`,error)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 const app = express();
 (async () => {
